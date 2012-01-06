79bff9e393f787a4d1afb1191c2275130781970d
Joshua Ball <JoshBBall@gmail.com>
2012-01-06 15:52:38 -0800

    Fixed full table scan bug.

--- lib/delayed/backend/active_record.rb
+++ lib/delayed/backend/active_record.rb
@@ -14,7 +14,7 @@ module Delayed
         before_save :set_default_run_at
 
         scope :ready_to_run, lambda {|worker_name, max_run_time|
-          where(['(run_at <= ? AND (locked_at IS NULL OR locked_at < ?) OR locked_by = ?) AND failed_at IS NULL', db_time_now, db_time_now - max_run_time, worker_name])
+          where(['(run_at <= ? AND ((locked_at IS NULL OR locked_at < ?) OR locked_by = ?)) AND failed_at IS NULL', db_time_now, db_time_now - max_run_time, worker_name])
         }
         scope :by_priority, order('priority ASC, run_at ASC')
 
